f1f3368421f1c7e218a47d57629838e8cff466cb
Volodymyr Anokhin <vlnn@simcorp.com>
2017-06-12 18:34:00 +0200

    area object added

---
+++ RF/Areas-Departments-Numbers/Area.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RF;
+
+namespace RF {
+    public class Area {
+        enum Areas {
+            North,
+            East,
+            South,
+            West
+        }
+        public int Region { get; private set; }
+        
+        public void Area(Areas area) {
+            Area = Area.area;
+        }
+    }
+}
