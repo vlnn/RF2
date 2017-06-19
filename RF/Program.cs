c44df0ee637d39b836fc93c6a6be2557cb485647
Volodymyr Anokhin <vlnn@simcorp.com>
2017-06-19 10:52:32 +0200

    Filler added to simplify the testing

--- RF/Program.cs
+++ RF/Program.cs
@@ -4,32 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using RFLibrary;
+using RF;
 
 namespace RF {
     class Program {
         static void Main() {
             Console.WriteLine("The RF base business layer started.");
-            var antenna1 = new Antenna();
-            antenna1.ProductID = "0001";
-            antenna1.InstallationID = "Svi001";
 
-            var antenna2 = new Antenna();
-            antenna1.ProductID = "0002";
-            antenna1.InstallationID = "Svi002";
-
-            var antenna3 = new Antenna();
-            antenna1.ProductID = "0003";
-            antenna1.InstallationID = "Svi003";
-
-            var Warez = new Warehouse();
-            Warez.Add(antenna1);
-            Warez.Add(antenna2);
-            Warez.RemoveLast();
-            Warez.Add(antenna3);
-
-            antenna1.Install();
-            antenna1.Maintain();
-            antenna1.Remove();
+            var filler = new Filler();
+            Warehouse Warez = filler.WarezFiller();
+           
             Console.WriteLine("There are " + Warez.Count() + " antennas in the storage.");
             Console.WriteLine("The RF base business layer stopped.");
             Console.ReadKey();
