2021d2c827bd8ef1635cf647c6306b52e9e5889c
Volodymyr Anokhin <vlnn@simcorp.com>
2017-06-27 07:56:22 +0200

    zero password fix

--- RF/GUI/Password.cs
+++ RF/GUI/Password.cs
@@ -7,12 +7,12 @@ using System.Threading.Tasks;
 namespace RF.GUI {
     class Password {
         private static List<char> pwd = new List<char>();
-        public static string ReadPwd() {            
+        public static string ReadPwd() {
             while (true) {
                 ConsoleKeyInfo i = Console.ReadKey(true);
                 if (i.Key == ConsoleKey.Enter) {
                     break;
-                } else if (i.Key == ConsoleKey.Backspace) {
+                } else if ((i.Key == ConsoleKey.Backspace) && (pwd.Count != 0)) {
                     pwd.RemoveAt(pwd.Count - 1);
                     Console.Write("\b \b");
                 } else {
